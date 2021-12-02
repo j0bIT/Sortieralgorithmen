a46d0b1a4156ca374a24e5082ed7de6b67abf0c1
Jonas <jonaspp471@gmail.com>
2021-12-02 10:53:43 +0100

    added quicksort

--- src/com/pohlmann/Main.java
+++ src/com/pohlmann/Main.java
@@ -1,15 +1,19 @@
 package com.pohlmann;
 
 import java.util.Arrays;
-import java.util.concurrent.Callable;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         int[] array = {4, 5, 2, 3, 6, 324, 23423423, 423, 4, 234, 34324, 34324};
-        int[] array1 = {3, 1, 4, 2};
 
+        System.out.println("Quicksort");
+        Tools.measureTime(() -> {
+            System.out.println(Arrays.toString(Quicksort.sort(array.clone())));
+            return null;
+        });
 
+        System.out.println("==================================================================");
 
         System.out.println("Insertionsort");
         Tools.measureTime(() -> {
---
+++ src/com/pohlmann/Quicksort.java
@@ -0,0 +1,39 @@
+package com.pohlmann;
+
+public class Quicksort {
+
+    public static int[] sort(int[] toSort) {
+        return sort(toSort, 0, toSort.length - 1);
+    }
+
+    public static int[] sort(int[] toSort, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(toSort, begin, end);
+
+            sort(toSort, begin, partitionIndex - 1);
+            sort(toSort, partitionIndex + 1, end);
+        }
+        return toSort;
+    }
+
+    private static int partition(int[] toSort, int begin, int end) {
+        int pivot = toSort[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (toSort[j] <= pivot) {
+                i++;
+
+                int swapTemp = toSort[i];
+                toSort[i] = toSort[j];
+                toSort[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = toSort[i + 1];
+        toSort[i + 1] = toSort[end];
+        toSort[end] = swapTemp;
+        return i + 1;
+    }
+
+}
